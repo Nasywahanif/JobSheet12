650d13e2c5f30b5ad08f961e8e5d73bdc2367786
dwadwadad@gmail.com <heydaristo@gmail.com>
2022-09-26 13:45:17 +0700

    revisi ke2

--- app/src/main/java/com/example/nasywa6/MainActivity.kt
+++ app/src/main/java/com/example/nasywa6/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.nasywa6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1 = findViewById<Button>(R.id.button)
+        val btn2 = findViewById<Button>(R.id.button2)
+
+        btn1.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+        btn2.setOnClickListener {
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
     }
 }
